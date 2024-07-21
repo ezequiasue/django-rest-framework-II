ed4d7ccd36f694cc77f0bb3610d9e030a5292516
Ezequias Silva Santos <phd.esantos@gmail.com>
2024-07-21 15:55:12 -0400

    Created Serializer for App Order /Product and category

--- product/models/product.py
+++ product/models/product.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     active = models.BooleanField(default=True)
-    categories = models.ManyToManyField('Category', related_name='products')
+    category = models.ManyToManyField('Category', related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
