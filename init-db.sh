1b9bb6e347a5440c6a37ed829386d4e7c9c6a458
Ezequias Silva Santos <phd.esantos@gmail.com>
2024-08-18 14:23:56 -0400

    Add GitHub Actions CI workflow for testing and Docker integration

--- init-db.sh
+++ init-db.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-# Adiciona a configuração de trust no pg_hba.conf
 echo "local all all trust" >> /var/lib/postgresql/data/pg_hba.conf
 echo "host all all 127.0.0.1/32 trust" >> /var/lib/postgresql/data/pg_hba.conf
 
-# Executa o comando padrão para iniciar o PostgreSQL
+
 exec "$@"
